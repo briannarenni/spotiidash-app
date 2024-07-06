79c97b5d5665e88491fd0b12d8883e03709c8a58
Brianna Renni <briannarenni@protonmail.com>
2024-07-06 16:34:04 -0700

    build: Add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@src': path.resolve(__dirname, './src'),
+      '@css': path.resolve(__dirname, './src/assets/css'),
+      '@js': path.resolve(__dirname, './src/js'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@blocks': path.resolve(__dirname, './src/lib/blocks'),
+      '@components': path.resolve(__dirname, './src/lib/components'),
+      '@containers': path.resolve(__dirname, './src/lib/containers'),
+    }
+  }
+});
